f956ed6861540cfbd86ab0fe1aaf2a2654fec7b1
mentlerd <mentlerd@gmail.com>
2026-07-18 19:31:57 +0200

    Adversary test against dynamic type resolution's current implementation
    The current implementation is entirely typename based, and as such fails
    for more complicated cases where the demangler and debug info don't agree
    on the spelling of a type associated with the vtable's symbol.

---
+++ lldb/test/API/lang/cpp/dynamic-value-complex-typename/TestDynamicValueComplexTypename.py
@@ -0,0 +1,30 @@
+import lldb
+import lldbsuite.test.lldbutil as lldbutil
+from lldbsuite.test.lldbtest import *
+
+
+class DynamicValueComplexTypename(TestBase):
+
+    def test(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break here", lldb.SBFileSpec("main.cpp")
+        )
+
+        self.checkVarType("simple", "Simple")
+        self.checkVarType("templated", "Templated<")
+        self.checkVarType("complicated", "Complicated<")
+        self.checkVarType("evil", "Evil<")
+
+    def checkVarType(self, name, type_prefix):
+        var = self.frame().FindVariable(name)
+        self.assertTrue(var.IsValid())
+
+        # Check if we recognized the dynamic type. Most names are unreasonably
+        # complex to match against exactly, but all should have a sane prefix
+        self.assertStartsWith(var.GetType().GetName(), type_prefix)
+
+        # Check if we resolved the compiler type too
+        marker = var.Dereference().GetChildAtIndex(0)
+        self.assertEqual(marker.GetName(), f"is_{name}")
+        self.assertEqual(marker.GetValueAsUnsigned(), 1)
